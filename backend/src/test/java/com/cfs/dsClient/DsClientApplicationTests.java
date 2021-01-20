a86ddef9173a7596059c6e7f898c66f523ba9903
fsclaudio <fsclaudio@gmail.com>
2021-01-19 22:48:35 -0300

    Project created

---
+++ backend/src/test/java/com/cfs/dsClient/DsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.dsClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
